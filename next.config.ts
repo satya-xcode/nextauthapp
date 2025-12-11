8e6da33099920ad4251d51e50b9f97530eaeb25c
SatyaXcode <satyaxcode@gmail.com>
2025-12-11 11:24:38 +0530

    skdfksfsd

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
+  env: {
+    NEXTAUTH_URL: "https://main.d13koukf3fjuf0.amplifyapp.com",
+    MONGODB_URI:
+      "mongodb+srv://satyaxcode_db_user:oXJkvi1teWGoZ5zm@tianyin-db.qvsj6a5.mongodb.net/?appName=tianyin-db",
+    AUTH_SECRET: "71fO2lkYpNaA1xq+/b98lpFLylzAbS++aPMH3rnBKYI=",
+    NEXTAUTH_SECRET: "71fO2lkYpNaA1xq+/b98lpFLylzAbS++aPMH3rnBKYI=",
+  },
 };
 
 export default nextConfig;
